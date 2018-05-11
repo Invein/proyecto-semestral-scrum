b9564844b2647c31ed5e3f5a41ff1be80b626ada
Gera <gtorres@rnviz.com>
2018-05-10 20:28:20 -0600

    DB connection, securityMiddleware, Config

---
+++ middlewares/securityMiddleware.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+
+
+function verifyToken(request, response, next) {
+  const token = request.body.token || request.query.token || request.headers['x-access-token'];
+  if (token) {
+    jwt.verify(token, config.get('api.key'), (err, decoded) => {
+      if (err) {
+        response.json({
+          error: true,
+          message: 'Llave incorrecta',
+          objs: {}
+        });
+      } else {
+        next();
+      }
+    });
+  } else {
+    response.json({
+      error: true,
+      message: 'Llave incorrecta',
+      objs: {}
+    });
+  }
+}
+
+module.exports = {
+  verifyToken
+};
