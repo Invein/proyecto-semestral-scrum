46b6a32d52ca548b5de8978ee7bc01c76f50326f
gera <gerardo.t.o@hotmail.com>
2018-05-25 16:58:35 -0600

    axios implemented in login form

--- public/scripts/loginForm.js
+++ public/scripts/loginForm.js
@@ -1,32 +1,55 @@
-$(document).ready(function () {
-    $("#login-form").validate({
-        rules: {
-            email: {
-                required: true,
-                email: true
-            },
-            password: {
-                required: true
-            }
-        },
-        messages: {
-            password: {
-                required: "Introduzca una contraseña"
-            },
-            email: {
-                required: "Introduzca un Email",
-                email: "E-mail no válido"
-            },
-            submitHandler: onSubmit
-        }
-    });
+// $(document).ready(function () {
+//     $("#login-form").validate({
+//         rules: {
+//             email: {
+//                 required: true,
+//                 email: true
+//             },
+//             password: {
+//                 required: true
+//             }
+//         },
+//         messages: {
+//             password: {
+//                 required: "Introduzca una contraseña"
+//             },
+//             email: {
+//                 required: "Introduzca un Email",
+//                 email: "E-mail no válido"
+//             },
+//             submitHandler: onSubmit
+//         }
+//     });
 
-    function onSubmit() {
-        const data = $("#login-form").serialize();
-        $.ajax({
-            type: "POST",
-            url: "http://localhost:3000/api/login",
-            data
+//     $("#login-form").onSubmit(function () {
+//         console.log("argumentos", arguments);
+//         alert("Al");
+//     })
+//     function onSubmit() {
+//         const data = $("#login-form").serialize();
+//         while (true) {
+//             console.log("HEY");
+//         }
+//         $.ajax({
+//             type: "POST",
+//             url: "http://localhost:3000/api/login",
+//             data
+//         });
+//     }
+// });
+
+$(document).ready(function () {
+    $("#login-btn").click(function () {
+        const values = {};
+        $("#login-form").find('input').each(function (i, tag) {
+            values[tag.name] = tag.value;
         });
-    }
+
+        axios.post("http://localhost:3000/api/login", values).then((response) => {
+            const { token } = response.data.objs;
+
+            cache = localStorage['scrum-app'];
+            document.location.replace("http://localhost:3000?token=" + token);
+        });
+    });
 });
