642d33ec808c7a3610e3282a83fad504d9331961
unknown <agarcia@rnviz>
2018-05-30 02:18:35 -0600

    Backlogs almost ready

---
+++ public/scripts/cardHistoryBehavior.js
@@ -0,0 +1,20 @@
+var cards = document.querySelectorAll(".card");
+var playing = false;
+cards.forEach(card => {
+    card.addEventListener('click', function () {
+        if (playing)
+            return;
+
+        playing = true;
+        anime({
+            targets: card,
+            scale: [{ value: 1 }, { value: 1.4 }, { value: 1, delay: 250 }],
+            rotateY: { value: '+=180', delay: 200 },
+            easing: 'easeInOutSine',
+            duration: 400,
+            complete: function (anim) {
+                playing = false;
+            }
+        });
+    });
+});
